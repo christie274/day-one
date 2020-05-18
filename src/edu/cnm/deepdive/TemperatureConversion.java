b8588bd102803bac63932651e4ef736a5746815b
Christie Ryan <cryan11@cnm.edu>
2020-05-18 16:19:12 -0600

    Check args for input values.

--- src/edu/cnm/deepdive/TemperatureConversion.java
+++ src/edu/cnm/deepdive/TemperatureConversion.java
@@ -3,22 +3,28 @@ package edu.cnm.deepdive;
 public class TemperatureConversion {
 
   public static void main(String[] args) {
-    double c = 100;
-    double f = 32;
-    System.out.println("Celsius " + c + " = " + convertC2F(c) + " Fahrenheit");
-    System.out.println("Fahrenheit " + f + " = " + convertF2C(f) + " Celsius");
+    if (args.length == 0) {
+      double tempCelsius = 100;
+      double tempFahrenheit = 32;
+      System.out.println("Celsius " + tempCelsius + " = " + convertC2F(tempCelsius) + " Fahrenheit");
+      System.out.println("Fahrenheit " + tempFahrenheit + " = " + convertF2C(tempFahrenheit) + " Celsius");
+    } else {
+      for (String arg : args) {
+        double tempCelsius = Double.parseDouble(arg);
+        System.out.println("Celsius " + tempCelsius + " = " + convertC2F(tempCelsius) + " Fahrenheit");
+      }
+    }
   }
 
   public static double convertF2C(double f) {
-    return (f - 32) * 9 / 5 + 32;
+    return (f - 32) * 9 / 5;
   }
 
   public static double convertC2F(double celsius) {
     return celsius * 9 / 5 + 32;
   }
 
-public static double convertC2FC(double fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
-}
 
 }
+
+
